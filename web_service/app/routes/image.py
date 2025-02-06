c63f3c20f38fdaec20531a13965763ce2a8b9238
VlaLee <vladivektin@mail.ru>
2025-02-06 22:56:02 +0300

    #fix did correct path for creating json and zip dataset

--- web_service/app/routes/image.py
+++ web_service/app/routes/image.py
@@ -7,8 +7,8 @@ from app.folderNotFoundError import FolderNotFoundError
 
 
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
-UPLOAD_FOLDER = 'images'
-ZIP_NAME = 'classification_dataset'
+UPLOAD_FOLDER_PATH = 'images/classification_dataset/train'
+UTILS_PATH = '../models/classification/utils'
 
 
 # функция проверки расширения загруженного файла
@@ -20,7 +20,7 @@ def create_zip(source_folder, zip_name):
     if not os.path.exists(source_folder):
         raise FolderNotFoundError(f"Папка {source_folder} не существует")
     
-    shutil.make_archive(zip_name, 'zip', source_folder)
+    shutil.make_archive(os.path.join(UTILS_PATH, zip_name), 'zip', source_folder)
 
 
 image_bp = Blueprint('image', __name__)
@@ -32,7 +32,7 @@ def image():
 
 @image_bp.route('/upload_images', methods = ['POST'])
 def upload_images():
-    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+    os.makedirs(UPLOAD_FOLDER_PATH, exist_ok=True)
     
     if not request.files:
         return jsonify({"message": "Нет загруженных файлов"}), 400
@@ -41,13 +41,15 @@ def upload_images():
     class_index = 0
     
     for class_id in request.files:
-        class_name = class_id.replace("[]", "")
+        class_name = class_id.replace("[]", "").lower()
         
         if class_name not in class_mapping:
             class_mapping[class_name] = class_index
             class_index += 1
             
-        class_folder = os.path.join(UPLOAD_FOLDER, f"class_{class_name}")
+        class_folder = os.path.join(UPLOAD_FOLDER_PATH, f"{class_name}")
+        print(class_folder)
+        
         os.makedirs(class_folder, exist_ok=True)
         
         for file in request.files.getlist(class_id):
@@ -58,11 +60,11 @@ def upload_images():
                 filepath = os.path.join(class_folder, file.filename)
                 file.save(filepath)
     
-    create_zip(UPLOAD_FOLDER, ZIP_NAME)
-    shutil.rmtree(UPLOAD_FOLDER)
+    create_zip('images', 'classification_dataset')
+    shutil.rmtree('images')
     
     json_data = json.dumps(class_mapping, ensure_ascii=False, indent=2)
-    json_filename = 'class_to_idx.json'
+    json_filename = os.path.join(UTILS_PATH, 'class_to_idx.json')
     with open(json_filename, "w", encoding='utf-8') as json_file:
         json_file.write(json_data)
             
